97728166a04ed7dd194055c39f83a61d33fb6431
Carlo Dee <109793453+carlodeeCrypton@users.noreply.github.com>
2022-08-29 22:38:47 +0800

    Update omni_rhode.mk

--- omni_rhode.mk
+++ omni_rhode.mk
@@ -7,19 +7,19 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from rhode device
 $(call inherit-product, device/motorola/rhode/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rhode
-PRODUCT_NAME := omni_rhode
+PRODUCT_NAME := twrp_rhode
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g(100)
 PRODUCT_MANUFACTURER := motorola
