b29415908eebd674cfa4e094141e9f1b848dac6d
Carlo Dee <109793453+carlodeeCrypton@users.noreply.github.com>
2022-08-29 22:39:38 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_rhode-user
-add_lunch_combo omni_rhode-userdebug
-add_lunch_combo omni_rhode-eng
+add_lunch_combo twrp_rhode-user
+add_lunch_combo twrp_rhode-userdebug
+add_lunch_combo twrp_rhode-eng
