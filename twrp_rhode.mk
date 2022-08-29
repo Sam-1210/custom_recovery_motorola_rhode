ae2ca4096b37695e840b1783ff34792179a74a1a
Carlo Dee <109793453+carlodeeCrypton@users.noreply.github.com>
2022-08-30 01:43:27 +0800

    Update twrp_rhode.mk

--- twrp_rhode.mk
+++ twrp_rhode.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/motorola/rhode/device.mk)
 
 # Inherit some common Twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-$(call inherit-product, vendor/twrp/config/gsm.mk)
+$(call inherit-product-if-exists, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rhode
