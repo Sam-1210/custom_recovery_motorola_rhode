4a2c077ddadd65c89a2ebefb7254aeb6796273e7
Carlo Dee <109793453+carlodeeCrypton@users.noreply.github.com>
2022-08-29 23:40:29 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -15,8 +15,9 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    bootctrl.sdm845 \
-    bootctrl.sdm845.recovery
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-service \
 
 PRODUCT_PACKAGES += \
     bootctrl.bengal
