4926198e7b6632c3b897f2e8e9fd2c82315f1ef1
Carlo Dee <carlodee.official@gmail.com>
2022-10-30 16:11:03 +0800

    Remove AB Partitions here, and reconfigure copy files for depmod.

--- device.mk
+++ device.mk
@@ -7,17 +7,6 @@
 
 LOCAL_PATH := device/motorola/rhode
 
-# A/B
-AB_OTA_PARTITIONS += \
-    boot \
-    system \
-    vendor \
-    product \
-    recovery \
-    vbmeta \
-    vbmeta_system \
-    dtbo
-
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
@@ -46,7 +35,6 @@ PRODUCT_PACKAGES += \
     bootctrl.bengal.recovery \
     bootctrl.bengal
 
-
 # Encryption
 PRODUCT_PACKAGES += \
     qcom_decrypt \
@@ -57,22 +45,15 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
     ro.bootimage.build.date.utc \
     ro.build.date.utc
 
-# Modules from vendor_boot.img
+# OEM otacerts
+PRODUCT_EXTRA_RECOVERY_KEYS += \
+    $(LOCAL_PATH)/security/otacert
+
+# Copy recovery depmod
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/adapter_class.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/adapter_class.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/cw2217b_fg_mmi.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/cw2217b_fg_mmi.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/mmi_annotate.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/mmi_annotate.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/mmi_charger.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/mmi_charger.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/mmi_discrete_charger.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/mmi_discrete_charger.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/mmi_discrete_charger_class.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/mmi_discrete_charger_class.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/mmi_info.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/mmi_info.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/modules.alias:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/modules.alias \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/modules.blocklist:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/modules.blocklist \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/modules.dep:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/modules.dep \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/modules.load:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/modules.load \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/modules.softdep:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/modules.softdep \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/qpnp_adaptive_charge.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/qpnp_adaptive_charge.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/rt_pd_manager.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/rt_pd_manager.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/tcpc_class.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/tcpc_class.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/tcpc_sgm7220.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/tcpc_sgm7220.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/sgm4154x_charger.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/sgm4154x_charger.ko
+    $(call find-copy-subdir-files,*.ko,device/motorola/rhode/prebuilt/modules/,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/) \
+    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/modules.alias:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/modules.alias \
+    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/modules.blocklist:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/modules.blocklist \
+    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/modules.dep:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/modules.dep \
+    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/modules.load:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/modules.load \
+    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/modules.softdep:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/modules.softdep 
